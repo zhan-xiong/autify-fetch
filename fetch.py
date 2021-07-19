11a57d4cff71d1134ff39f39fd1da21e884bd27f
Zhan Xiong Chin <zhanxiong.chin@gmail.com>
2021-07-19 01:25:47 +0000

    Parse arguments

--- fetch.py
+++ fetch.py
@@ -1,3 +1,12 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Downloads a given set of URLs")
+    parser.add_argument("-m", "--metadata", action="store_true")
+    parser.add_argument("urls", nargs="*")
+    return parser.parse_args()
 
 if __name__ == "__main__":
-    print("Hello World!")
+    args = parse_args()
+    print(args.urls)
+    print(args.metadata)
